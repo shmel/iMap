7a3e24683c6695952e84168b9958f3621bd4b73a
pskym <pskym@spatialsys.com>
2014-10-20 10:39:11 -0400

    10/20/2014
    pie chart with counts, no legend yet

--- basicviewer/src/modules/custom/vFire/vFire.js
+++ basicviewer/src/modules/custom/vFire/vFire.js
@@ -10,12 +10,17 @@
  *  Note: It seems when working with map layer events (e.g. "onClick"),
  *  in order to work with modules, dojo/aspect after() or before() functions should be used.
  */
-define(["dojo/_base/declare", "dojo/dom-construct", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/on", "dijit/registry"
-    , "dojo/text!./templates/vFire.html", "dojo/_base/lang"
-    , "dojo/dom", "dojo/query", "dojox/charting/Chart", "dojox/charting/themes/Claro", "../../core/utilities/maphandler"
-    , "dojox/charting/plot2d/Pie", "dojox/charting/action2d/Tooltip", "dojox/charting/action2d/MoveSlice", "xstyle/css!./css/vFire.css"],
-    function(declare, domConstruct, WidgetBase, TemplatedMixin, on, regstry, template, lang
-            , dom, query, Chart, theme, mapHandler, PiePlot, Tooltip, MoveSlice){
+define(["dojo/_base/declare", "dojo/dom-construct", "dojo/on", "dojo/text!./templates/vFire.html", "dojo/_base/lang", "dojo/dom", "dojo/query", "dojo/_base/array"
+    , "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/registry"
+    , "dojox/charting/Chart", "dojox/charting/themes/Claro", "dojox/charting/plot2d/Pie", "dojox/charting/action2d/Tooltip", "dojox/charting/action2d/MoveSlice"
+    , "../../core/utilities/maphandler"
+    , "esri/tasks/QueryTask", "esri/tasks/query" , "esri/tasks/StatisticDefinition"
+    , "xstyle/css!./css/vFire.css"],
+    function(declare, domConstruct, on, template, lang, dom, query, arrayUtil
+            , WidgetBase, TemplatedMixin, registry
+            , Chart, theme, PiePlot, Tooltip, MoveSlice
+            , mapHandler
+            , QueryTask, Query, StatisticDefinition){
         return declare([WidgetBase, TemplatedMixin], {
             templateString: template
             , baseClass: "vFireDiv"
@@ -24,56 +29,86 @@ define(["dojo/_base/declare", "dojo/dom-construct", "dijit/_WidgetBase", "dijit/
             , constructor: function(arcgs){
                 declare.safeMixin(this, arcgs);
                 this.map = mapHandler.map;
-                this.chartData = [
-                    {x: 1, y: 10, tooltip: "Not Exercised Yet", text:"", color: "rgb(2, 92, 230)", mylabel: "Not Exercised Yet"},
-                    {x: 1, y: 10, tooltip: "In Process",  text: "", color: "rgb(255, 255, 2)", mylabel: "In Process" },
-                    {x: 1, y: 20, tooltip: "In Process, Valve Not Found", text: "", color: "rgb(170, 2, 230)", mylabel: "In Process, Valve Not Found" },
-                    {x: 1, y: 20, tooltip: "In Process, Repair Required", text: "", color: "rgb(230, 2, 2)", mylabel: "In Process, Repair Required" },
-                    {x: 1, y: 20, tooltip: "Exercise Completed", text: "",  color: "rgb(57, 169, 2)", mylabel: "Exercise Completed" },
-                    {x: 1, y: 20, tooltip: "Requires GPS Location", text: "", color: "rgb(137, 91, 70)", mylabel: "Requires GPS Location" }
-                ];
-                /*var queryvFire = new Query();
-                var vFireQueryTask = new QueryTask("http://prod1.spatialsys.com/arcgis/rest/services/CharlesUtilities/water_vep_valves_fs/MapServer/0/query");
-                queryvFire.where = "1 = 1";
-                queryvFire.outFields = ["Status", "Total Count"];
-                vFireQueryTask.execute(queryvFire, lang.hitch(this, function(resultsvFire){
-                    this.chartData = arrayUtils.map(resultsvFire.features, function(featurevFire){
-                        return{
-                            x: 1,
-                            y: featurevFire.attributes["Total Count"],
-                            tooltip: featurevFire.attributes["Status"],
-                            text: ""
-                        }
-                    })
-                }));*/
-
             }
             , postCreate: function () {
                 this.inherited(arguments);
             }
             , startup: function () {
                 this.inherited(arguments);
-                var pieChart = new Chart("vFireChartDiv", {
-                    title: "V-FIRE Valve Status",
-                    titlePos: "top",
-                    titleFont: "normal normal normal 12pt Arial",
-                    titleFontColor: "black"
-                });
-                pieChart.setTheme(theme);
-                pieChart.addPlot("default", {
-                    type: PiePlot,
-                    radius: 200,
-                    fontColor: "black",
-                    labelOffset: -20
-                });
-                pieChart.addSeries("Pie Chart - FY XXXX", this.chartData);
-                var tip = new Tooltip(pieChart, "default", {
-                    text: lang.hitch(this, function(o){
-                        return this.chartData[o.index].tooltip + "<br/>" + this.chartData[o.index].y
-                    })
-                });
-                var anim = new MoveSlice(pieChart, "default", {});
-                pieChart.render();
+                var countDiv = dom.byId("completedCountDiv");
+                var valveTrans = [
+                    {
+                        "color": "rgb(2, 92, 230)",
+                        "tooltip": "Not Exercised Yet"
+                    },
+                    {
+                        "color": "rgb(255, 255, 2)",
+                        "tooltip": "In Process"
+                    },
+                    {
+                        "color": "rgb(170, 2,230)",
+                        "tooltip": "In Process, Valve Not Found"
+                    },
+                    {
+                        "color": "rgb(230, 2, 2)",
+                        "tooltip": "In Process, Repair Required"
+                    },
+                    {
+                        "color": "rgb(57, 169, 2)",
+                        "tooltip": "Exercise Completed"
+                    },
+                    {
+                        "color": "rgb(137, 91, 70)",
+                        "tooltip": "Requires GPS Location"
+                    }
+                ];
+                var queryTest = new Query();
+                var testQueryTask = new QueryTask("http://prod1.spatialsys.com/arcgis/rest/services/CharlesUtilities/water_vep_valves_fs/MapServer/0/query");
+                var stats = new StatisticDefinition();
+                stats.statisticType = "count";
+                stats.onStatisticField = "OBJECTID";
+                stats.outStatisticFieldName = "CountByValveStatus";
+                queryTest.where = "1 = 1";
+                queryTest.outStatistics = [stats];
+                queryTest.groupByFieldsForStatistics = ["VEP_STATUS"];
+                testQueryTask.execute(queryTest, lang.hitch(this, function(resultsTest){
+                    this.chartData = arrayUtil.map(resultsTest.features, function(featureTest) {
+                        var vStatus = featureTest.attributes["VEP_STATUS"];
+                        var vCount = featureTest.attributes["CountByValveStatus"];
+                        if (vStatus == 4) {
+                            countDiv.innerHTML = vCount + " Valves completed to date";
+                        };
+                        return {
+                            x: 1,
+                            y: vCount,
+                            tooltip: valveTrans[vStatus].tooltip,
+                            color: valveTrans[vStatus].color,
+                            text: vCount
+                        }
+                    });
+                    var pieChart = new Chart("vFireChartDiv", {
+                        title: "V-FIRE Valve Status",
+                        titlePos: "top",
+                        titleFont: "normal normal normal 12 pt Arial",
+                        titleFontColor: "black"
+                    });
+                    pieChart.setTheme(theme);
+                    pieChart.addPlot("default", {
+                        type: PiePlot,
+                        radius: 200,
+                        fontColor: "black",
+                        labelOffset: -20
+                    });
+                    pieChart.addSeries("V-FIRE Valve Status", this.chartData);
+                    var tip = new Tooltip(pieChart, "default", {
+                        text: lang.hitch(this, function(o) {
+                            return this.chartData[o.index].tooltip + "<br/>" + this.chartData[o.index].y
+                        })
+                    });
+                    var anim = new MoveSlice(pieChart, "default", {});
+                    pieChart.render();
+                }));
             }
+
     });
 });

337b3d1b03cc8c05dac0988c911bd907dd390663
pskym <pskym@spatialsys.com>
2014-12-03 13:47:15 -0500

    12/3/2014
    pie slice adjustable

--- basicviewer/src/modules/custom/vFire/vFire.js
+++ basicviewer/src/modules/custom/vFire/vFire.js
@@ -14,14 +14,14 @@ define(["dojo/_base/declare", "dojo/dom-construct", "dojo/on", "dojo/text!./temp
     , "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/registry", "dijit/form/DateTextBox", "dijit/form/Button"
     , "dojox/charting/Chart", "dojox/charting/plot2d/Pie", "dojox/charting/action2d/Tooltip", "dojox/charting/action2d/MoveSlice", "dojox/charting/widget/SelectableLegend", "dojox/charting/SimpleTheme", "dojox/charting/widget/Legend"
     , "../../core/utilities/maphandler"
-    , "esri/tasks/QueryTask", "esri/tasks/query" , "esri/tasks/StatisticDefinition"
+    , "esri/tasks/QueryTask", "esri/tasks/query" , "esri/tasks/StatisticDefinition", "esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol", "esri/tasks/Geoprocessor", "esri/renderers/SimpleRenderer", "esri/graphic", "esri/layers/ImageParameters", "esri/tasks/JobInfo", "esri/layers/FeatureLayer"
     , "dojo/domReady!"
     , "xstyle/css!./css/vFire.css"],
     function(declare, domConstruct, on, template, lang, dom, query, arrayUtil, locale, parser, date
             , WidgetBase, TemplatedMixin, registry, DateTextBox, Button
             , Chart, PiePlot, Tooltip, MoveSlice, SelectableLegend, SimpleTheme, pLegend
             , mapHandler
-            , QueryTask, Query, StatisticDefinition){
+            , QueryTask, Query, StatisticDefinition, SimpleMarkerSymbol, SimpleLineSymbol, GP, SimpleRenderer, Graphic, ImageParameters, JobInfo, FeatureLayer){
         return declare([WidgetBase, TemplatedMixin], {
             templateString: template
             , baseClass: "vFireDiv"
@@ -61,6 +61,8 @@ define(["dojo/_base/declare", "dojo/dom-construct", "dojo/on", "dojo/text!./temp
                 this.inherited(arguments);
             }
             , startup: function () {
+                convertStart = "01-01-1800";
+                convertEnd = "12-31-4000";
                 this.inherited(arguments);
                 //call private function _queryMap with whereClause "1 = 1" initially
                 this._queryMap("1 = 1");
@@ -82,15 +84,16 @@ define(["dojo/_base/declare", "dojo/dom-construct", "dojo/on", "dojo/text!./temp
                     var $today = new Date();
                     var $yesterday = new Date($today);
                     $yesterday.setDate($today.getDate() - 1);
-                    var convertYesterday = widgetStart.format($yesterday, {
+                    convertStart = widgetStart.format($yesterday, {
                         datePattern: "yyyy-MM-dd",
                         selector: "date"
                     });
-                    var convertEnd = widgetEnd.format($today, {
+                    convertEnd = widgetEnd.format($today, {
                         datePattern: "yyyy-MM-dd",
                         selector: "date"
                     });
-                    this._queryMap("VEP_LAST_EDIT > date '" + convertYesterday + "' AND VEP_LAST_EDIT <= date'" + convertEnd + "'");
+                    this._clearLayer(featureLayer);
+                    this._queryMap("VEP_LAST_EDIT > date '" + convertStart + "' AND VEP_LAST_EDIT <= date'" + convertEnd + "'");
                 }));
                 var lastWeek = new Button ({
                     label: "Last Week",
@@ -107,15 +110,16 @@ define(["dojo/_base/declare", "dojo/dom-construct", "dojo/on", "dojo/text!./temp
                     $Sunday.setDate($today.getDate() - 7 - $Day);
                     var $Saturday = $oneWeekBackEnd;
                     $Saturday.setDate($today.getDate() - $Day - 1);
-                    var convertSunday = widgetStart.format($Sunday, {
+                    convertStart = widgetStart.format($Sunday, {
                         datePattern: "yyyy-MM-dd",
                         selector: "date"
                     });
-                    var convertSaturday = widgetEnd.format($Saturday, {
+                    convertEnd = widgetEnd.format($Saturday, {
                         datePattern: "yyyy-MM-dd",
                         selector: "date"
                     });
-                    this._queryMap("VEP_LAST_EDIT > date '" + convertSunday + "' AND VEP_LAST_EDIT <= date'" + convertSaturday + "'");
+                    this._clearLayer(featureLayer);
+                    this._queryMap("VEP_LAST_EDIT > date '" + convertStart + "' AND VEP_LAST_EDIT <= date'" + convertEnd + "'");
                 }));
                 var submit = new Button ({
                     label: "Submit",
@@ -124,14 +128,15 @@ define(["dojo/_base/declare", "dojo/dom-construct", "dojo/on", "dojo/text!./temp
                 }, "submitCustomDate");
                 submit.startup();
                 on(submit, "click", lang.hitch(this, function(){
-                    var convertStart = widgetStart.format(widgetStart.value, {
+                    convertStart = widgetStart.format(widgetStart.value, {
                         datePattern: "yyyy-MM-dd",
                         selector: "date"
                     });
-                    var convertEnd = widgetEnd.format(widgetEnd.value, {
+                    convertEnd = widgetEnd.format(widgetEnd.value, {
                         datePattern: "yyyy-MM-dd",
                         selector: "date"
                     });
+                    this._clearLayer(featureLayer);
                     this._queryMap("VEP_LAST_EDIT > date '" + convertStart +"' AND VEP_LAST_EDIT <= date'" + convertEnd + "'");
                 }));
                 var Original = new Button ({
@@ -141,8 +146,34 @@ define(["dojo/_base/declare", "dojo/dom-construct", "dojo/on", "dojo/text!./temp
                 }, "Original");
                 Original.startup();
                 on(Original, "click", lang.hitch(this, function(){
+                    convertStart = "01-01-1800";
+                    convertEnd = "12-31-4000";
+                    this._clearLayer(featureLayer);
                     this._queryMap("1 = 1")
                 }));
+                //GP service
+                gp = new GP("http://prod1.spatialsys.com/arcgis/rest/services/DevVFIRE/OverUnderOneThousandValves/GPServer/Over%20Under%20One%20Thousand%20Valves");
+                gp.setOutSpatialReference({wkid:26985});
+                conjugator = {
+                    "State_Date" : convertStart,
+                    "End_Date" : convertEnd,
+                    "Valve_Count" : "0",
+                    "Valve_Limit" : "1000",
+                    "Valve_Status" : "Exercise Completed",
+                    "Valve_Feature_Class" : "D:\\CharlesCountyUtilities\\Data\\WaterValveExerciseProgram\\water_valve_exercise_program_operational.gdb\\wExerciseSystemValves",
+                    "Subdivision_Feature_Class" : "D:\\CharlesCountyUtilities\\Data\\WaterValveExerciseProgram\\CharlesCounty_Parcels_Meters_201402.gdb\\Cadastral\\Subdivision_Plats"
+                };
+                featureLayer = null;
+                //GP button test
+                /*var pieQuery = new Button ({
+                    label: "pieQuery",
+                    type: "button",
+                    name: "pieQuery"
+                }, "pieQuery");
+                pieQuery.startup();
+                on(pieQuery, "click", lang.hitch(this, function(){
+                    this._pieQuery();
+                }));*/
             }
             , //private function to which passes query as whereClause then creates the pie chart and legend to view
             _queryMap: function(whereClause) {
@@ -190,6 +221,7 @@ define(["dojo/_base/declare", "dojo/dom-construct", "dojo/on", "dojo/text!./temp
                         titleFontColor: "black",
                         legend: this.chartData.legend
                     });
+
                     var myTheme = new SimpleTheme({
                         chart: {
                             stroke: null,
@@ -219,6 +251,27 @@ define(["dojo/_base/declare", "dojo/dom-construct", "dojo/on", "dojo/text!./temp
                     });
                     var anim = new MoveSlice(pieChart, "default", {});
                     pieChart.render();
+                    pieChart.connectToPlot("default", lang.hitch(this, function(evt) {
+                        if(evt.type == "onclick") {
+                            //reconfigure conjugator based on pie slice
+                            //clear previous layor
+                            if (this.pieQueryinProcess == false) {
+                                this.pieQueryinProcess = true;
+                                this._clearLayer(featureLayer);
+                                var startDate = convertStart;
+                                var endDate = convertEnd;
+                                var count = evt.run.data[evt.index].y;
+                                var c = count.toString();
+                                var status = evt.run.data[evt.index].tooltip;
+                                conjugator.State_Date = startDate;
+                                conjugator.End_Date = endDate;
+                                conjugator.Valve_Count = c;
+                                conjugator.Valve_Status = status;
+                                //console.log(conjugator);
+                                this._pieQuery();
+                            }
+                        }
+                    }));
                     var legend = dijit.byId("Legend");
                     if(legend != undefined) {
                         legend.destroyRecursive(true);
@@ -232,12 +285,111 @@ define(["dojo/_base/declare", "dojo/dom-construct", "dojo/on", "dojo/text!./temp
                 }));
             }
             , //private function to delete the domNodes associated with the pie chart and legend
-            _clearResults : function(){
+            _clearResults: function() {
                 var clearPieChart = dom.byId("vFireChartDiv");
                 clearPieChart.innerHTML = null;
                 var clearCompletedCount = dom.byId("completedCountDiv");
                 clearCompletedCount.innerHTML = null;
             }
+            , //private function zoom to layer
+            _zoomToLayer: function(featurelayer) {
+                //var featurelayer = this.map.getLayer(layername)
+                var query = new Query();
+                var random = (new Date()).getTime();
+                query.where = random + " = " + random;
+                query.outSpatialReference = this.map.spatialReference;
+                featurelayer.queryFeatures(query, lang.hitch(this, function (featureSet) {
+                    var data = [];
+                    if (featureSet && featureSet.features && featureSet.features.length > 0) {
+                        data = featureSet.features;
+                    }
+                    var zoomExtent = esri.graphicsExtent(data);
+                    this.map.setExtent(zoomExtent.expand(1.2));
+                }));
+            }
+            , //private function to execute pieQuery
+            _pieQuery: function() {
+                this.map.graphics.clear();
+                gp.submitJob(conjugator, lang.hitch(this, this._displayFMainsLayer)/*, function statusCallback(jobInfo) {
+                    console.log(jobInfo.jobStatus);
+                }*/);
+                mapHandler.ShowLoadingIcon();
+            }
+            , //private function display features
+            _displayFMainsLayer: function(jobInfo) {
+                document.body.style.cursor = "default";
+                if(jobInfo.jobStatus == "esriJobSucceeded") {
+                    var mapurl = "http://prod1.spatialsys.com/arcgis/rest/services/DevVFIRE/OverUnderOneThousandValves/MapServer/jobs/" + jobInfo.jobId;
+                }
+                var FLayerURL = mapurl + "/0";
+                featureLayer = FeatureLayer(FLayerURL, {
+                    id: "FLayer",
+                    mode: FeatureLayer.MODE_SNAPSHOT//,
+                    //outFields: ["*"]
+                });
+                var simplePointJson = {
+                    "type": "simple",
+                    "label": "",
+                    "description": "",
+                    "symbol": {
+                        "type": "esriSMS",
+                        "style": "esriSMSCircle",
+                        "color": [0,255,0,255],
+                        "width": 32,
+                        "outline": {
+                            "color" : [255,255,255,255],
+                            "width" : 2,
+                            "type" : "esriSLS",
+                            "style" : "esriSLSSolid"
+                        }
+                    }
+                };
+                var simplePolyJson = {
+                    "type": "simple",
+                    "label": "",
+                    "description": "",
+                    "symbol": {
+                        "type": "esriSFS",
+                        "style": "esriSFSSolid",
+                        "color": [0,255,0,235],
+                        "outline": {
+                            "color" : [255,255,255,255],
+                            "width" : 0,
+                            "type" : "esriSLS",
+                            "style" : "esriSLSSolid"
+                        }
+                    }
+                };
+                var count = parseInt(conjugator.Valve_Count);
+                var limit = parseInt(conjugator.Valve_Limit);
+                if(count <= limit) {
+                    var simpleJson = simplePointJson
+                }
+                else {
+                    var simpleJson = simplePolyJson
+                }
+                var rend = new SimpleRenderer(simpleJson);
+                featureLayer.setRenderer(rend);
+                this.map.addLayer(featureLayer);
+                this._zoomToLayer(featureLayer);
+                mapHandler.HideLoadingIcon();
+                this.pieQueryinProcess = false;
+            }
+            , //private function to clear layer
+            _clearLayer: function(featureLayer) {
+                var layersList = ["FLayer"];
+                this.map.graphics.clear();
+                for(var b = this.map.graphicsLayerIds.length -1; b > 0; --b) {
+                    var layer = this.map.getLayer(this.map.graphicsLayerIds[b]);
+                    //console.log(layer.id + ' ' + layer.opacity + ' ' + layer.visible);
+                    if (layersList.indexOf(layer.id) > -1 ) {
+                        //Remove Layer
+                        this.map.removeLayer(layer)
+                    }
+                }
+            }
+            , //private process
+            pieQueryinProcess : false
 
     });
 });
